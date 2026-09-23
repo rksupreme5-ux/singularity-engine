5a67ecc1f597058230fd0a4a270fe34b7a4ea41d
Rishu kumar sharma <rksupreme5@gmail.com>
2026-09-23 16:12:58 +0000

    Added missing CSS import

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import './style.css';
 import * as THREE from 'three';
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
